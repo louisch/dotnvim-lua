64501385fb5f09f3084b0691d07b65be4a60c488
Louis Chan <louisch@fastmail.com>
2023-01-17 13:22:08 +0000

    Add keymaps for LSP

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -52,7 +52,84 @@ local config = function ()
         vim.api.nvim_command("tabprev")
       end,
       "Go to previous tab"
-    }
+    },
+
+    ["do"] = {
+      vim.diagnostic.open_float,
+      "Open Diagnostics"
+    },
+    dp = {
+      vim.diagnostic.goto_prev,
+      "Diagnostics goto prev"
+    },
+    dn = {
+      vim.diagnostic.goto_next,
+      "Diagnostics goto next"
+    },
+    dq = {
+      vim.diagnostic.setloclist,
+      "Diagnostics setloclist"
+    },
+
+    gd = {
+      vim.lsp.buf.definition,
+      "Go to definition",
+    },
+    gD = {
+      vim.lsp.buf.declaration,
+      "Go to declaration",
+    },
+    gi = {
+      vim.lsp.buf.implementation,
+      "Go to implementation",
+    },
+    gr = {
+      vim.lsp.buf.references,
+      "Go to references",
+    },
+
+    kh = {
+      vim.lsp.buf.hover,
+      "See hover information",
+    },
+    ks = {
+      vim.lsp.buf.signature_help,
+      "See signature help",
+    },
+    kt = {
+      vim.lsp.buf.type_definition,
+      "See type definition",
+    },
+
+    wa = {
+      vim.lsp.buf.add_workspace_folder,
+      "Add workspace folder",
+    },
+    wr = {
+      vim.lsp.buf.remove_workspace_folder,
+      "Remove workspace folder",
+    },
+    wl = {
+      function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end,
+      "List workspace folders",
+    },
+
+    cr = {
+      vim.lsp.buf.rename,
+      "Rename symbol",
+    },
+    cf = {
+      function()
+        vim.lsp.buf.format({ async = true})
+      end,
+      "Format buffer",
+    },
+    ca = {
+      vim.lsp.buf.code_action,
+      "Code Action",
+    },
   }, { prefix = "<leader>" })
 
   -- Abbreviation for vertical help
