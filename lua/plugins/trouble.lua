fc89cf7c6701746c92ab79be28732c802d3cc7fb
Louis Chan <louis.chan@lidl.com.hk>
2023-02-02 14:59:19 +0800

    Bind trouble keymaps with which-key

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,24 +1,19 @@
 local config = function ()
   require("trouble").setup {}
 
-  vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
-    {silent = true, noremap = true}
-  )
-  vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
-    {silent = true, noremap = true}
-  )
-  vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
-    {silent = true, noremap = true}
-  )
-  vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-    {silent = true, noremap = true}
-  )
-  vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-    {silent = true, noremap = true}
-  )
-  vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-    {silent = true, noremap = true}
-  )
+  local wk = require("which-key")
+
+  wk.register({
+    x = {
+      name = "Trouble",
+      x = { "<cmd>TroubleToggle<cr>", "Toggle Trouble" },
+      w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Trouble (Workspace Diagnostics)" },
+      d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Trouble (Document Diagnostics)" },
+      l = { "<cmd>TroubleToggle loclist<cr>", "Trouble (loclist)" },
+      q = { "<cmd>TroubleToggle quickfix<cr>", "Trouble (Quick Fixes)" },
+      r = { "<cmd>TroubleToggle lsp_references<cr>", "Trouble (References)" },
+    },
+  }, { prefix = "<leader>" })
 end
 
 return {
